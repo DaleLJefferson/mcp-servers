d6983d5e08625a11e1dff894e52d2f6e65ecc6a9
Dale Jefferson <dale@dalejefferson.com>
2025-03-23 23:54:50 +0000

    Simplify git commit instructions

--- packages/git/git.ts
+++ packages/git/git.ts
@@ -15,7 +15,7 @@ const server = new McpServer(
     instructions: `
 Before adding files, use the status tool to check if there are any unexpected changes.
 Before using the diff tool, use the status tool so you know to diff staged or unstaged changes.
-Before committing, use the diff tool to review changes first. Then use the commit tool to commit the changes.
+Before committing, use the diff tool to review changes first.
 `,
   }
 );
